1563c244533b8c175508dfe38f4abd346ec36df4
Ananta2545 <chatterjeeanata091@gmail.com>
2024-11-01 18:27:09 +0530

    added farmerHome

--- frontend/src/pages/FarmerHome/FarmerHome.jsx
+++ frontend/src/pages/FarmerHome/FarmerHome.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import Weather from '../../components/Weather'
+import React from "react";
+import Chart from "../../components/chart/Chart.jsx";
+import Featured from "../../components/featured/Featured.jsx";
+import List from "../../components/list/List.jsx";
+import Navbar from "../../components/navbar/Navbar.jsx";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Widget from "../../components/widget/Widget.jsx"
+import "./FarmerHome.scss";
 
-const FarmerHome = () => {
+const Home = () => {
   return (
-    <div className="container-farmer">
-      <Weather/>
+    <div className="home">
+      <>
+        <Sidebar />
+        <div className="homeContainer">
+          <Navbar />
+          <div className="widgets">
+            <Widget type="customer" />
+            <Widget type="order" />
+            <Widget type="earnings" />
+            <Widget type="balance" />
+          </div>
+          <div className="charts">
+            <Featured />
+            <Chart title="Last 6 months (Revenue)" aspect={2 / 1} />
+          </div>
+          {/* <div className="listContainer">
+            <div className="listTitle">Latest Transactions</div>
+            <List />
+          </div> */}
+        </div>
+      </>
     </div>
-  )
-}
+  );
+};
 
-export default FarmerHome
+export default Home;

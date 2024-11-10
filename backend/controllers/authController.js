1dbcbf8d812c0c1ab54b6fb5d9e18220e1e1070d
Ananta2545 <chatterjeeanata091@gmail.com>
2024-11-10 15:03:26 +0530

    fixing deployment issue

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -61,7 +61,7 @@ export const signin = async (req, res) => {
 
         // Set token in cookie
         return res
-            .cookie('token', token)  // Setting cookie})
+            .cookie('token', token, {secure: true})  // Setting cookie})
             .status(200)
             .json({ message: "Logged in successfully", token, role: user.role });
     } catch (err) {

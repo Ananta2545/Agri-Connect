8b58c7034e050702e5fc48a0f9790501e6e76024
Ananta2545 <chatterjeeanata091@gmail.com>
2024-11-10 15:54:27 +0530

    fixing deployment issue

--- frontend/src/pages/Authentication/Authentication.jsx
+++ frontend/src/pages/Authentication/Authentication.jsx
@@ -69,7 +69,6 @@ const Authentication = ({ setUserRole }) => {
         password,
         role,
       },{withCredentials: true})
-      // document.cookie = `token=${response.data.token}; path=/;`;
       toast.success(response.data.message)
       setUserRole(response.data.role)
       navigate(role === 'farmer'? '/farmer_home': 'expert_home')

3fb33bc37e59c2bb1aa09ff8824c91e78399df72
Ananta2545 <chatterjeeanata091@gmail.com>
2024-11-10 14:51:11 +0530

    fixing deployment issue

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 8000;
 // Set up middleware
 app.use(cookieParser());
 app.use(cors({
-    origin: 'https://weather-b6e3.vercel.app/',
+    origin: 'https://weather-b6e3.vercel.app',
     credentials: true,
 }));
 app.use(express.json());
@@ -39,7 +39,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'https://weather-b6e3.vercel.app/',  // Adjust this as needed for your frontend URL
+        origin: 'https://weather-b6e3.vercel.app',  // Adjust this as needed for your frontend URL
         credentials: true,
         methods: ["GET", "POST"]
     },
